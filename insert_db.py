c8c861fdb1520e377e611d858c02346e05aec660
inoue_r <chru21072@g.nihon-u.ac.jp>
2024-06-28 10:04:31 +0900

    twitchの合計再生回数を取得できるようにした

--- insert_db.py
+++ insert_db.py
@@ -1,37 +1,37 @@
-import os
-import psycopg2
 import json
+import psycopg2
+import sys
+import os
 
-from dotenv import load_dotenv
-load_dotenv()
-
-PGHOST = os.getenv('PGHOST')
-PGPORT = os.getenv('PGPORT')
-PGDATABASE = os.getenv('PGDATABASE')
-PGUSER = os.getenv('PGUSER')
-PGPASSWORD = os.getenv('PGPASSWORD')
-
-def insert_data():
-    print(PGPORT)
-    conn = psycopg2.connect(
-        host=PGHOST,
-        port=PGPORT,
-        dbname=PGDATABASE,
-        user=PGUSER,
-        password=PGPASSWORD,
+def insert_data_to_db(json_file, db_host, db_port, db_name, db_user, db_password):
+    connect = psycopg2.connect(
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        dbname=db_name,
+        port=db_port
     )
+    cur = connect.cursor()
 
-    cur = conn.cursor()
-        
-    # 接続確認用のクエリを実行
-    cur.execute("SELECT version();")
-    db_version = cur.fetchone()
-    print(f"Connected to database. PostgreSQL version: {db_version[0]}")
-    
-    # 接続を閉じる
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+
+    for item in data:
+        cur.execute("""
+            INSERT INTO twitch_videos (game_title, game_id, total_views) VALUES (%s, %s, %s)
+        """, (item['game_title'], item['game_id'], item['total_views']))
+
+    connect.commit()
     cur.close()
-    conn.close()
-    
+    connect.close()
 
-if __name__ == '__main__':
-    insert_data()
+if __name__ == "__main__":
+    # json_file = sys.argv[1]
+    json_file = 'twitch_top_games.json'
+    PGHOST = os.getenv('PGHOST')
+    PGPORT = os.getenv('PGPORT')
+    PGDATABASE = os.getenv('PGDATABASE')
+    PGUSER = os.getenv('PGUSER')
+    PGPASSWORD = os.getenv('PGPASSWORD')
+
+    insert_data_to_db(json_file, PGHOST, PGPORT, PGDATABASE, PGUSER, PGPASSWORD)
--- steam/spiders/items.py
+++ steam/spiders/items.py
@@ -1,28 +1,66 @@
-# -*- coding: utf-8 -*-
-import os
-import json
 import scrapy
-import requests
-from datetime import date, datetime, timedelta
+import json
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
 class ItemsSpider(scrapy.Spider):
-    name = "items"
-    # allowed_domains = ["api.twitch.tv"]
-    # start_urls = ["https://api.twitch.tv"]
+    name = 'items'
+    allowed_domains = ['api.twitch.tv']
+    base_url = 'https://api.twitch.tv/helix/games/top'
+    headers = {
+        'Client-ID': CLIENT_ID,
+        'Authorization': f'Bearer {CLIENT_SECRET}'
+    }
 
-    def __init__(self, target_date=None, *args, **kwargs):
-        super(ItemsSpider, self).__init__(*args, **kwargs)
-        if target_date:
-            self.target_date = target_date
+    custom_settings = {
+        'FEED_FORMAT': 'json',
+        'FEED_URI': 'twitch_top_games.json'
+    }
+
+    def start_requests(self):
+        # url = self.base_url + '?first=100'
+        url = self.base_url + '?first=5'
+        yield scrapy.Request(url, headers=self.headers, callback=self.parse_games)
+
+    def parse_games(self, response):
+        top_games = json.loads(response.body)
+        for game in top_games['data']:
+            game_title = game['name']
+            game_id = game['id']
+
+            yield scrapy.Request(
+                f'https://api.twitch.tv/helix/videos?sort=views&game_id={game_id}&first=50',
+                headers=self.headers,
+                callback=self.parse_videos,
+                meta={'game_title': game_title, 'game_id': game_id, 'total_views': 0}
+            )
+
+    def parse_videos(self, response):
+        game_title = response.meta['game_title']
+        game_id = response.meta['game_id']
+        total_views = response.meta['total_views']
+
+        videos = json.loads(response.body)
+
+        for video in videos['data']:
+            total_views += video['view_count']
+
+        if 'pagination' in videos and 'cursor' in videos['pagination']:
+            next_page = f'https://api.twitch.tv/helix/videos?sort=views&game_id={game_id}&first=50&after={videos["pagination"]["cursor"]}'
+            yield scrapy.Request(
+                next_page,
+                headers=self.headers,
+                callback=self.parse_videos,
+                meta={'game_title': game_title, 'game_id': game_id, 'total_views': total_views}
+            )
         else:
-            self.target_date = date.today().strftime('%y-%m-%d')
-
-    @ classmethod
-    def from_crawler(cls, crawler, *args, **kwargs):
-        spider = super(ItemsSpider, cls).from_crawler(crawler, *args, **kwargs)
-        crawler.signals.connect(spider.engine_stopped,
-                                signal=scrapy.signals.engine_stopped)
-        return spider
-
-    def parse(self, response):
-        pass
+            yield {
+                'game_title': game_title,
+                'game_id': game_id,
+                'total_views': total_views
+            }
