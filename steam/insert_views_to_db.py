39b3cf740d5aab468bdc28186f892a3516c6c834
inoue_r <chru21072@g.nihon-u.ac.jp>
2024-07-25 01:25:14 +0900

    データベースにsteamデータを直接入れるように変更

--- steam/insert_views_to_db.py
+++ steam/insert_views_to_db.py
@@ -18,13 +18,37 @@ def insert_views_to_db(json_file, db_host, db_port, db_name, db_user, db_passwor
         )
         cur = connect.cursor()
 
-        with open(json_file, 'r') as f:
+        with open(json_file, 'r', encoding='utf-8') as f:
             data = json.load(f)
 
         for item in data:
+            # Delete existing records from steam_data and steam_data_genres
+            cur.execute("DELETE FROM steam_data_genres WHERE steam_game_id = '%s'", (item['steam_id'],))
+            cur.execute("DELETE FROM steam_data WHERE steam_game_id = '%s'", (item['steam_id'],))
+
+            # Insert data into steam_data table
+            cur.execute("""
+                INSERT INTO steam_data (
+                    steam_game_id, twitch_game_id, game_title, webpage_url, img_url, price,
+                    is_single_player, is_multi_player, is_device_windows, is_device_mac
+                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """, (
+                item['steam_id'], item['twitch_id'], item['game_title'], item['webpage_url'],
+                item['img_url'], item['price'], item['is_single_player'], item['is_multi_player'],
+                item['is_device_windows'], item['is_device_mac']
+            ))
+
+            # Insert data into steam_data_genres table
+            for genre in item['genres']:
+                cur.execute("""
+                    INSERT INTO steam_data_genres (steam_game_id, genre_id)
+                    SELECT %s, genre_id FROM genres WHERE genre_id = %s
+                """, (item['steam_id'], genre['id']))
+
             cur.execute("""
                 INSERT INTO game_views (get_date, game_title, twitch_id, steam_id, total_views) VALUES (%s, %s, %s, %s, %s)
             """, (datetime.datetime.now(), item['game_title'], item['twitch_id'], item['steam_id'], item['total_views']))
+
 
         connect.commit()
         cur.close()
--- steam/spiders/items.py
+++ steam/spiders/items.py
@@ -21,9 +21,13 @@ class ItemsSpider(scrapy.Spider):
     base_url = 'https://api.twitch.tv/helix/games/top'
     token = get_access_token()
 
-    headers = {
+    twitch_headers = {
         'Client-ID': CLIENT_ID,
-        'Authorization': f'Bearer {token}'
+        'Authorization': f'Bearer {token}',
+    }
+
+    steam_headers = {
+        'Accept-Language': 'ja'
     }
 
     custom_settings = {
@@ -42,11 +46,9 @@ class ItemsSpider(scrapy.Spider):
 
     def start_requests(self):
         url = self.base_url + '?first=100'
-        yield scrapy.Request(url, headers=self.headers, callback=self.parse_twitch, meta={'top_games_index': 0, 'top_games': []})
+        yield scrapy.Request(url, headers=self.twitch_headers, callback=self.parse_twitch, meta={'top_games_index': 0, 'top_games': []})
 
     def parse_twitch(self, response):
-        # top_games = response.meta['top_games']  # JSON形式に変換
-
         top_games_json = json.loads(response.body)
         cursor = top_games_json.get('pagination', {}).get('cursor', None)
         top_games = top_games_json['data']
@@ -60,7 +62,7 @@ class ItemsSpider(scrapy.Spider):
             url = f'https://api.twitch.tv/helix/games/top?first=100&after={cursor}'
             yield scrapy.Request(
                 url,
-                headers=self.headers,
+                headers=self.twitch_headers,
                 callback=self.parse_twitch,
                 meta={'top_games_index': top_games_index, 'top_games': top_games}
             )
@@ -68,10 +70,10 @@ class ItemsSpider(scrapy.Spider):
             url = 'https://api.steampowered.com/ISteamApps/GetAppList/v2/'
             yield scrapy.Request(
                 url,
+                headers=self.steam_headers,
                 callback=self.parse_games,
                 meta={'top_games': top_games}
             )
-
 
     def parse_games(self, response):
         steam_games_json = json.loads(response.body)
@@ -81,22 +83,71 @@ class ItemsSpider(scrapy.Spider):
         for game in top_games:
             game_title = game['name']
             twitch_id = game['id']
-            url = f'https://api.twitch.tv/helix/videos?sort=views&game_id={twitch_id}&period=day&first=100'
 
             if game_title in steam_games_dict and game_title != 'Just Chatting':
                 steam_id = steam_games_dict[game_title]
+                steam_url = f'https://store.steampowered.com/api/appdetails?appids={steam_id}&cc=jp'
                 yield scrapy.Request(
-                    url,
-                    headers=self.headers,
-                    callback=self.parse_videos,
-                    meta={'game_title': game_title, 'twitch_id': twitch_id, 'steam_id': steam_id}
+                    steam_url,
+                    headers=self.steam_headers,
+                    callback=self.parse_steam_details,
+                    meta={'game_title': game_title, 'twitch_id': twitch_id, 'steam_id': steam_id},
+                    dont_filter=True
                 )
 
+    def parse_steam_details(self, response):
+        game_title = response.meta['game_title']
+        twitch_id = response.meta['twitch_id']
+        steam_id = response.meta['steam_id']
+
+        steam_details = json.loads(response.body)
+        game_data = steam_details.get(str(steam_id), {}).get('data', {})
+        is_success = steam_details.get(str(steam_id), {}).get('success', False)
+
+        genres = game_data.get('genres', [])
+        webpage_url = 'https://store.steampowered.com/app/' + str(steam_id)
+        img_url = game_data.get('header_image', f'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/{steam_id}/header.jpg')
+        price = 0
+        if 'price_overview' in game_data and 'final' in game_data['price_overview']:
+            price = game_data['price_overview']['final'] / 100
+        is_single_player = any(category['id'] == 2 for category in game_data.get('categories', []))
+        is_multi_player = any(category['id'] == 1 for category in game_data.get('categories', []))
+        is_device_windows = game_data.get('platforms', {}).get('windows', False)
+        is_device_mac = game_data.get('platforms', {}).get('mac', False)
+
+        twitch_url = f'https://api.twitch.tv/helix/videos?sort=views&game_id={twitch_id}&period=day&first=100'
+        if is_success == True:
+            yield scrapy.Request(
+            twitch_url,
+            headers=self.twitch_headers,
+            callback=self.parse_videos,
+            meta={'game_title': game_title,
+                  'twitch_id': twitch_id,
+                  'steam_id': steam_id,
+                  'genres': genres,
+                  'webpage_url': webpage_url,
+                  'img_url': img_url,
+                  'price': price,
+                  'is_single_player': is_single_player,
+                  'is_multi_player': is_multi_player,
+                  'is_device_windows': is_device_windows,
+                  'is_device_mac': is_device_mac,
+                 }
+            )
+        
 
     def parse_videos(self, response):
         game_title = response.meta['game_title']
         twitch_id = response.meta['twitch_id']
         steam_id = response.meta['steam_id']
+        genres = response.meta['genres']
+        webpage_url = response.meta['webpage_url']
+        img_url = response.meta['img_url']
+        price = response.meta['price']
+        is_single_player = response.meta['is_single_player']
+        is_multi_player = response.meta['is_multi_player']
+        is_device_windows = response.meta['is_device_windows']
+        is_device_mac = response.meta['is_device_mac']
         total_views = 0
 
         videos = json.loads(response.body)
@@ -108,6 +159,13 @@ class ItemsSpider(scrapy.Spider):
             'game_title': game_title,
             'twitch_id': twitch_id,
             'steam_id': steam_id,
-            'total_views': total_views
+            'total_views': total_views,
+            'genres': genres,
+            'webpage_url': webpage_url,
+            'img_url': img_url,
+            'price': price,
+            'is_single_player': is_single_player,
+            'is_multi_player': is_multi_player,
+            'is_device_windows': is_device_windows,
+            'is_device_mac': is_device_mac,
         }
-
