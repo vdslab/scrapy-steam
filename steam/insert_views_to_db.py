0f632391317f8ebe4ff32434ecb76456f3e701fd
inoue_r <chru21072@g.nihon-u.ac.jp>
2024-08-06 01:04:53 +0900

    データベース追加処理時間を減少

--- steam/insert_views_to_db.py
+++ steam/insert_views_to_db.py
@@ -21,34 +21,57 @@ def insert_views_to_db(json_file, db_host, db_port, db_name, db_user, db_passwor
         with open(json_file, 'r', encoding='utf-8') as f:
             data = json.load(f)
 
-        for item in data:
-            # Delete existing records from steam_data and steam_data_genres
-            cur.execute("DELETE FROM steam_data_genres WHERE steam_game_id = '%s'", (item['steam_id'],))
-            cur.execute("DELETE FROM steam_data WHERE steam_game_id = '%s'", (item['steam_id'],))
+        steam_data_values = []
+        steam_data_genres_values = []
+        game_views_values = []
 
-            # Insert data into steam_data table
-            cur.execute("""
-                INSERT INTO steam_data (
-                    steam_game_id, twitch_game_id, game_title, webpage_url, img_url, price,
-                    is_single_player, is_multi_player, is_device_windows, is_device_mac
-                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            """, (
+        for item in data:
+            # Prepare values for steam_data table
+            steam_data_values.append((
                 item['steam_id'], item['twitch_id'], item['game_title'], item['webpage_url'],
                 item['img_url'], item['price'], item['is_single_player'], item['is_multi_player'],
                 item['is_device_windows'], item['is_device_mac']
             ))
 
-            # Insert data into steam_data_genres table
+            # Prepare values for steam_data_genres table
             for genre in item['genres']:
-                cur.execute("""
-                    INSERT INTO steam_data_genres (steam_game_id, genre_id)
-                    SELECT %s, genre_id FROM genres WHERE genre_id = %s
-                """, (item['steam_id'], genre['id']))
+                steam_data_genres_values.append((item['steam_id'], genre['id']))
 
-            cur.execute("""
-                INSERT INTO game_views (get_date, game_title, twitch_id, steam_id, total_views) VALUES (%s, %s, %s, %s, %s)
-            """, (datetime.datetime.now(), item['game_title'], item['twitch_id'], item['steam_id'], item['total_views']))
+            # Prepare values for game_views table
+            game_views_values.append((
+                datetime.datetime.now(), item['game_title'], item['twitch_id'], item['steam_id'], item['total_views']
+            ))
 
+        # Insert or update steam_data table
+        cur.executemany("""
+            INSERT INTO steam_data (
+                steam_game_id, twitch_game_id, game_title, webpage_url, img_url, price,
+                is_single_player, is_multi_player, is_device_windows, is_device_mac
+            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            ON CONFLICT (steam_game_id) DO UPDATE SET
+                twitch_game_id = EXCLUDED.twitch_game_id,
+                game_title = EXCLUDED.game_title,
+                webpage_url = EXCLUDED.webpage_url,
+                img_url = EXCLUDED.img_url,
+                price = EXCLUDED.price,
+                is_single_player = EXCLUDED.is_single_player,
+                is_multi_player = EXCLUDED.is_multi_player,
+                is_device_windows = EXCLUDED.is_device_windows,
+                is_device_mac = EXCLUDED.is_device_mac
+        """, steam_data_values)
+
+        # Delete and insert data into steam_data_genres table
+        cur.executemany("DELETE FROM steam_data_genres WHERE steam_game_id = %s", [(item[0],) for item in steam_data_values])
+        cur.executemany("""
+            INSERT INTO steam_data_genres (steam_game_id, genre_id)
+            SELECT %s, genre_id FROM genres WHERE genre_id = %s
+        """, steam_data_genres_values)
+
+        # Insert data into game_views table
+        cur.executemany("""
+            INSERT INTO game_views (get_date, game_title, twitch_id, steam_id, total_views)
+            VALUES (%s, %s, %s, %s, %s)
+        """, game_views_values)
 
         connect.commit()
         cur.close()
